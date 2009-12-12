1651ce7862514b60ddfb141dd23a16120a49d376
Chris Sloan <chris@chrissloan.info>
2009-12-11 22:27:13 -0500

    Added more widgets: kaCenter, KaTagger, kaScroller. Also fixed the auto timed slider

--- src/kaloo.js
+++ src/kaloo.js
@@ -15,36 +15,39 @@ License: Open Source MIT Licence
 	$.fn.kaTab = function(options){
 		
 		var options = $.extend({ // set the default options
+			defaultTab: ":first", // the detauld tab
 			tabsClass: "tabs", // the tabs
 			activeClass: "active", // what the tab should look like active
 			hideAll: false, // should all content be hidden
 			hasCloser: false, // has an internal link to close the tab
 			internalCloserClass: "tab_closer", // internal link closer class
-			timer: { // if tabs should cycle through like a semi slide show (requires jQuery Timers Plugin)
+			useAJAX: false, // should other tabs be loaded via AJAX
+			jxContentClass: "jx_content", // where the ajax content is to load
+			onComplete: function(){}, // any functions needed to be done after completion of AJAX call
+			timer: { // if tabs should cycle through like a semi slide show (requires jQuery Timer Plugin)
 				isTimed: false,
 				interval: null
 			}
 		}, options ||{});
-		
+
 		// Set the objects
 		var element = $(this);
+		var elementID = element.attr("id");
 		var tabs = element.find("ul:first." + options.tabsClass + " li a");
-		
+
 		var nextIndex = 1; // for timer object
-	
-		var toShow = ":first"; // setting this as default as it's most likely to happen
-		 
+		
 		if(location.hash){// Determin if a hash exists in url or not
 			tabs.each(function(index){
 				if($(this).attr("href") == location.hash){ // does the hash equal any div ID's in the given set?
-					toShow = location.hash;
-					options.timer.isTimed ? nextIndex = index : null; // for the timer
+					options.defaultTab = location.hash;
 					return false; // jump out
-				}else{
-					toShow = ":first";
 				}
 			});
-		}
+		}else{
+		  if (options.defaultTab != ':first' && options.defaultTab.lastIndexOf("#") == -1)
+        options.defaultTab = "#" + options.defaultTab;
+    }
 
 		if (options.hasCloser){ // set all the internal closers up
 			element.find("." + options.internalCloserClass)
@@ -57,16 +60,25 @@ License: Open Source MIT Licence
 		}
 		
 		if(options.hideAll){ // should all the tabs be hidden on page load
-			hideTabContent();
+			tabs.each(function(index){
+				element.find($(this).attr("href")).hide();
+			});
 		}else{
-			if (toShow == ":first"){ // make the first tab active
+			if (options.defaultTab == ":first" || options.useAJAX){ // make the first tab active
 				tabs.each(function(index){
 					index == 0 ? $(this).parent().addClass(options.activeClass) : null; // set the first tab with active class
-					index != 0 ?	element.find($(this).attr("href")).hide() : null; // hide all other tab content but first
+					if(options.useAJAX){
+						if (index == 0){
+							$(this).attr("id", "tab_" + tabs.index($(this)));
+							showAJAX($(this).attr("jxURL"), options.jxContentClass, tabs.index($(this)));
+						}
+					}else{
+						index != 0 ?	element.find($(this).attr("href")).hide() : null; // hide all other tab content but first
+					}
 				});
 			}else{ // make the tab with hash active and show accordingly
 				tabs.each(function(index){
-					if(toShow == $(this).attr("href")){
+					if(options.defaultTab == $(this).attr("href")){
 						$(this).parent().addClass(options.activeClass);
 					}else{
 						element.find($(this).attr("href")).hide();
@@ -89,23 +101,57 @@ License: Open Source MIT Licence
 		// event handler
 		tabs.click(
 			function(event){
-				var theDiv = $(this).attr("href"); // grab the url for corresponding div
+				if(options.useAJAX){
+					if($(this).attr("id")){ // if an ID on the link has been set
+						showContent(tabs.index($(this))); // show based on DOM already loaded
+					}else{ // show based on AJAX call
+						$(this).attr("id", "tab_" + tabs.index($(this)));
+						showAJAX($(this).attr("jxURL"), options.jxContentClass, tabs.index($(this)));
+					}
+				}else{
+					var theDiv = $(this).attr("href"); // grab the url for corresponding div
+					tabs.each(function(index){
+						element.find($(this).attr("href")).hide();
+					});
+					element.find(theDiv).show();
+				}
+			
 				tabs.parent().removeClass(options.activeClass);
 				$(this).parent().addClass(options.activeClass);
-				hideTabContent();
-				element.find(theDiv).show();
-				
-				options.timer.isTimed ? $(element).stopTime() : null; // stop the timer if there is one
-				
 				return false; // stop the page jump
 			}
 		);
+		
+		function showContent(tab_index){ // For AJAX loaded content already in the DOM
+			$("."+ elementID + "_loaded").each(function(){
+				$(this).hide();
+			});
+			$("."+ elementID + "_content_" + tab_index).show();
+		}
+		
+		function showAJAX(url, content, tab_index){
+			$("."+ elementID + "_loaded").each(function(){
+				$(this).hide();
+			});
+			$(element).find(".jx_loader").show(); // make it so it works on multiple boxes!!
+			$(element).find("." + content).show().fadeTo("normal", 0.2).load(
+				url, 
+				{}, 
+				function(){
+					$(element).find(".jx_loader").hide();
+					$(element).find("." + content).hide().clone().show().insertAfter(this).removeClass(content).addClass(elementID +"_loaded " + elementID + "_content_" + tab_index).fadeTo("fast", 1); // clone the loaded div and place into the DOM for no AJAX re-call
+					options.onComplete.call(self); // Any functions to call after completion
+				}
+			);
+			
+		}
 		
 		function hideTabContent(){ // to cut down on duplication of code
 			tabs.each(function(index){
 				element.find($(this).attr("href")).hide();
 			});
 		}
+		
 	}
 	
 	//---------------------------------
@@ -160,8 +206,14 @@ License: Open Source MIT Licence
 		
 		setupSlider(); // run and create the visuals
 		
-		nextButton.click(slideNext);
-		prevButton.click(slidePrevious);
+		nextButton.click(function(){
+			slideNext();
+			options.timer.isTimed ? $(element).stopTime() : null; // stop the timer if there is one
+		});
+		prevButton.click(function(){
+			slidePrevious();
+			options.timer.isTimed ? $(element).stopTime() : null; // stop the timer if there is one
+		});
 		
 		if(options.autoSlide){ // for sliding directly to an item
 		  items.each(function(index){
@@ -200,7 +252,6 @@ License: Open Source MIT Licence
 				currentPosition = currentPosition + wSlideBy;
 			}
 			
-			options.timer.isTimed ? $(element).stopTime() : null; // stop the timer if there is one
 			return false;
 		}
 		
@@ -257,11 +308,14 @@ License: Open Source MIT Licence
 	//---------------------------------
 	// EXPAND CLASS
 	// use: $(selectors).kaTab(options)
+	//
+	// Expands/contracts specified divs
 	//---------------------------------
 	$.fn.kaPand = function(options){
 		
 		var options = $.extend({ // set the default options
 			togglerClass: "kaPander",
+			expandedClass: "expanded",
 			hiddenElementClass: "hidden_element",
 			toggledClass: "is_showing",
 			hideText: "...show less"
@@ -280,4 +334,142 @@ License: Open Source MIT Licence
 		);
 	}
 	
+	//-----------------------------
+	// CENTER OBJECTS CLASS
+	// use: $(selectors).kaCenter(options)
+	//
+	// Centers objects within viewport
+	//-----------------------------
+	$.fn.kaCenter = function(options) {
+	   var pos = {
+      sTop : function() {
+        return window.pageYOffset
+        || document.documentElement && document.documentElement.scrollTop
+        ||  document.body.scrollTop;
+      },
+      wHeight : function() {
+        return window.innerHeight
+        || document.documentElement && document.documentElement.clientHeight
+        || document.body.clientHeight;
+      },
+			sLeft : function() {
+        return window.pageXOffset
+        || document.documentElement && document.documentElement.scrollLeft
+        ||  document.body.scrollLeft;
+      },
+      wWidth : function() {
+        return window.innerWidth
+        || document.documentElement && document.documentElement.clientWidth
+        || document.body.clientWidth;
+      }
+    };
+    return this.each(function(index) {
+      if (index == 0) {
+        var $this = $(this);
+        var elHeight = $this.height();
+				var elWidth = $this.width();
+        var elTop = pos.sTop() + (pos.wHeight() / 2) - (elHeight / 2);
+				var elLeft = pos.sLeft() + (pos.wWidth() / 2) - (elWidth / 2);	
+        $this.css({
+          position: 'absolute',
+          marginTop: '0',
+          top: elTop,
+					left: elLeft
+        });
+      }
+    });
+  };
+
+	//-----------------------------
+	// SIMPLE TAGGING
+	// use: $(selectors).kaCenter(options)
+	//
+	// Works like Delecious
+	//-----------------------------
+	$.fn.kaTagger = function(options) {
+		var options = $.extend({ // set the default options
+			tagsContainerClass: "tags_list",
+			selectedClass: "selected",
+			togglerClass: "toggle_link",
+			moreTagsID: "more_tags",
+			fakeInput: "fake_input"
+		}, options ||{});
+
+		var tags = $("." + options.tagsContainerClass).find(".tag");
+		var tagsInput = $(this);
+		var fakeInput = $("#" + options.fakeInput); // So that you cant type tags in
+		var inputValue = tagsInput.val();
+		var fakeValue = fakeInput.val();
+		var toggleLink = $("." + options.tagsContainerClass).find("." + options.togglerClass);
+		var moreTags = $("#" + options.moreTagsID);
+
+		checkInput(); // lets select all the stuff first
+
+		toggleLink.click(function(){
+			$(moreTags).toggle();
+			$(moreTags).css("display") != "none" ? toggleLink.html("&laquo; show less") : toggleLink.html("show more &raquo;");
+		});
+
+		tags.click(function(){
+			var tagText = $(this).text();
+
+			if($(this).hasClass(options.selectedClass)){
+				if (inputValue == tagText){
+					tagsInput.val("");
+					fakeInput.val("");
+				}else if (inputValue.beginsWith(tagText)){
+					tagsInput.val(inputValue.replace(tagText + ", ", ""));
+					fakeInput.val(inputValue.replace(tagText + ", ", ""));
+				}else{
+					tagsInput.val(inputValue.replace(", " + tagText, ""));
+					fakeInput.val(inputValue.replace(", " + tagText, ""));
+				}
+				$(this).removeClass(options.selectedClass);
+
+			}else{
+				inputValue == "" ? tagsInput.val(tagText) : tagsInput.val(inputValue + ", " + tagText);
+				fakeValue == "" ? fakeInput.val(tagText) : fakeInput.val(inputValue + ", " + tagText);
+				$(this).addClass(options.selectedClass);
+			}
+
+			inputValue = tagsInput.val();
+			fakeValue = fakeInput.val();
+		});
+
+		function checkInput(){
+
+			var currentInput = inputValue.replace(/, /g, ","); //Remove space, but keep the 
+			currentInput = currentInput.replace(/ /g, '_'); //For any tag that might be multiple words with a space
+	    currentInput = currentInput.replace(/,/g, ' '); //Ready the transformed string for the array
+			var inputArray = new Array();
+			inputArray = currentInput.split(" ");
+
+			$.each(inputArray, function(n, value){
+				tags.each(function(){
+					$(this).text().replace(/ /g, '_') == value ? $(this).addClass("selected") : null;
+				});
+			});
+		}
+
+	}
+	
+	//-----------------------------
+	// WINDOW SCROLLER BUILDER
+	// use: $(selectors).kaScroller(options)
+	// 
+	// For scrolling window to points on page - requires ScrollTo JS
+	//-----------------------------
+	$.fn.kaScroller = function(options){
+
+		var options = $.extend({
+			duration: 800 // how quick is it
+		}, options || {} );
+
+		$(this).click(function(){
+			$.scrollTo($(this).attr("href"), options.duration);
+			return false;
+		});
+
+	}
+
 })(jQuery);
